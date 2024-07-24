95f82dea946b49967b98818acda84c3f1be94c0e
Barchi Abderrahman <abdoudeveloper5@gmail.com>
2024-07-24 21:09:05 +0100

    Update project

--- lib/core/services/myServices.dart
+++ lib/core/services/myServices.dart
@@ -9,10 +9,10 @@ class MyServices extends GetxService {
   Future<MyServices> init() async {
     await Firebase.initializeApp(
         options: const FirebaseOptions(
-            apiKey: "AIzaSyDvtJDRdl-LRlF4UwPGSwbkB8I1aQ746Rg",
-            appId: "1:74390462209:android:5e60df6703dab6e36f0500",
-            messagingSenderId: "932275925874",
-            projectId: "delivery-app-db8f1"));
+            apiKey: "",
+            appId: "",
+            messagingSenderId: "",
+            projectId: ""));
     sharedPreferences = await SharedPreferences.getInstance();
     return this;
   }
